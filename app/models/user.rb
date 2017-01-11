451931b14abe8de435ed65cb941000f8e3669092
Dan Finelli <dan.finelli@hotmail.com>
2017-01-10 16:20:28 -0800

    added default profile image and fixed spelling mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     has_many :sales, dependent: :destroy
 
     has_many :jointables, dependent: :destroy
-    has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/assets/missing.png'
+    has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: 'http://i.imgur.com/7Yc9GZf.png'
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
     ratyrate_rateable 'rating'
