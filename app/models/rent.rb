d6cb40b88b7d9ad6dd0989e4650ebcdcefb42cfd
Dan Finelli <dan.finelli@hotmail.com>
2017-01-12 11:23:38 -0800

    added image arry to models

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,3 +1,4 @@
 class Rent < ApplicationRecord
   belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111102605) do
+ActiveRecord::Schema.define(version: 20170112191502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,11 @@ ActiveRecord::Schema.define(version: 20170111102605) do
     t.decimal  "price"
     t.string   "address"
     t.string   "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.integer  "user_id"
     t.integer  "zipcode"
+    t.string   "images",     default: [],              array: true
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
@@ -105,8 +106,9 @@ ActiveRecord::Schema.define(version: 20170111102605) do
     t.integer  "bed"
     t.integer  "bath"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "images",     default: [],              array: true
     t.index ["user_id"], name: "index_rents_on_user_id", using: :btree
   end
 
@@ -117,9 +119,10 @@ ActiveRecord::Schema.define(version: 20170111102605) do
     t.decimal  "price"
     t.integer  "zipcode"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.integer  "buyer_id"
+    t.string   "images",     default: [],              array: true
     t.index ["user_id"], name: "index_sales_on_user_id", using: :btree
   end
 
