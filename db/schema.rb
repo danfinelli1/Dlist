c6d1ad78f63dc32c533d04da61f30d87cbe32014
dkingdon <daniel.kingdon@gmail.com>
2017-01-13 09:23:08 -0800

    changed hardcoded use id on sales seed data, deleted some not needed comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170112075006) do
-
+ActiveRecord::Schema.define(version: 20170112193305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,9 +69,8 @@ ActiveRecord::Schema.define(version: 20170112075006) do
     t.text     "body"
     t.integer  "conversation_id"
     t.integer  "user_id"
-    t.boolean  "read",            default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
